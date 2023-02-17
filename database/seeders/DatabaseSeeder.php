429a6ade0489a49d412df9568c4db44674268427
vladyslavMartynenkoSmarttek <vladyslav.martynenko@smartteksas.com>
2023-02-17 20:44:19 +0200

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             RoleAndPermissionSeeder::class,
+            CreateAdminUserSeeder::class,
+            CreateUserRoleSeeder::class,
         ]);
     }
 }
